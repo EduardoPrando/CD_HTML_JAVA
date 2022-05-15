cd0b708dcd2fe3f30b23993911f1e5bebbc69000
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2022-05-15 17:16:19 -0300

    fix: Repository imports

--- backend/src/main/java/com/scc/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/scc/backend/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.scc.backend.repositories;
 
 import com.scc.backend.models.UserModel;
 
+// import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
