1419bd85b913758d0e486f05b3c74c0faa4e0bd9
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2022-05-15 17:15:58 -0300

    Fix: Service Imports

--- backend/src/main/java/com/scc/backend/services/UserService.java
+++ backend/src/main/java/com/scc/backend/services/UserService.java
@@ -1,37 +1,40 @@
 package com.scc.backend.services;
 
+import java.util.List;
 import java.util.Optional;
 
-import javax.transaction.Transactional;
+// import javax.transaction.Transactional;
 
 import com.scc.backend.models.UserModel;
 import com.scc.backend.repositories.UserRepository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.data.domain.Page;
+// import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
   
+  @Autowired
   final UserRepository userRepository;
 
   public UserService(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
-  @Transactional
+  // @Transactional
   public UserModel save(UserModel user) {
     return userRepository.save(user);
   }
 
-  @Transactional
+  // @Transactional
   public void delete(UserModel user) {
     userRepository.delete(user);
   }
 
-  public Page<UserModel> findAll(Pageable pageable) {
-    return userRepository.findAll(pageable);
+  public List<UserModel> findAll() {
+    return userRepository.findAll();
   }
 
   public Optional<UserModel> findById(String id) {
