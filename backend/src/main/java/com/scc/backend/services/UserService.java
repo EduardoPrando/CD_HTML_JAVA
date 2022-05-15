7484bf5092add096add7cf5e7e6d6f2d7520dcf5
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2022-05-15 17:23:07 -0300

    Fix: clean comments

--- backend/src/main/java/com/scc/backend/services/UserService.java
+++ backend/src/main/java/com/scc/backend/services/UserService.java
@@ -3,14 +3,10 @@ package com.scc.backend.services;
 import java.util.List;
 import java.util.Optional;
 
-// import javax.transaction.Transactional;
-
 import com.scc.backend.models.UserModel;
 import com.scc.backend.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.data.domain.Page;
-// import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,12 +19,10 @@ public class UserService {
     this.userRepository = userRepository;
   }
 
-  // @Transactional
   public UserModel save(UserModel user) {
     return userRepository.save(user);
   }
 
-  // @Transactional
   public void delete(UserModel user) {
     userRepository.delete(user);
   }
